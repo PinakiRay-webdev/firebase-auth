a2899dda135f8057f6a508a78484aa583b71eea8
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-11-22 20:54:26 +0530

    UI enhancements

--- src/Authentication/SignUp.jsx
+++ src/Authentication/SignUp.jsx
@@ -1,108 +1,220 @@
 import React from "react";
+import loginbg from "../assets/loginBG.svg";
+import microsoft from "../assets/microsoft.svg";
+import google from "../assets/google.svg";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { ToastContainer, toast } from "react-toastify";
-import { auth } from "../../firebase-config/firebase";
-import "react-toastify/dist/ReactToastify.css";
-
+import {auth} from '../../utils/firebase/firebase'
 const SignUp = () => {
-  const navigate = useNavigate();
-
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
     reset,
   } = useForm();
 
-  const handleFormData = async (data) => {
-    toast.loading("Form is submitting", { theme: "dark" });
+  const navigate = useNavigate();
+
+  const submit = async (data) => {
+    toast.loading("signing....", { theme: "dark" });
     await new Promise((resolve) => {
       setTimeout(() => {
         resolve();
       }, 1500);
     }).then(() => {
+      toast.dismiss();
       createUserWithEmailAndPassword(auth, data.mail, data.password)
-        .then((userCredential) => {
-          // Signed up
-          const user = userCredential.user;
-          toast.dismiss()
-          toast.success('logged in successfully' , {theme : 'dark'})
-          localStorage.setItem('accountCredentials' , JSON.stringify({
-            mail : user.email
-          }))
+        .then((userCredentials) => {
+          const user = userCredentials.user;
+          toast.success("Account registered succcessfully", { theme: "dark" });
+          reset();
+          localStorage.setItem(
+            "accountCredentials",
+            JSON.stringify({
+              mail: user.email,
+            })
+          );
           setTimeout(() => {
-            navigate('/users')
+            navigate("/");
           }, 1000);
         })
         .catch((error) => {
-          const errorMessage = error.message;
           toast.dismiss();
-          toast.error(errorMessage , {theme : 'dark'})
+          toast.error(error.message, { theme: "dark" });
         });
-      reset();
     });
   };
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <form onSubmit={handleSubmit(handleFormData)}>
-        <label className="text-sm font-semibold text-zinc-700" htmlFor="">
-          Email Address
-        </label>
-        <br />
-        <input
-          {...register("mail", {
-            required: {
-              value: true,
-              message: "This feild is required",
-            },
-          })}
-          className="outline-none ring-1 ring-zinc-500 rounded-md py-2 px-3 my-2"
-          type="email"
-          placeholder="abc@example.com"
-        />
-        {errors.mail && (
-          <p className="text-xs text-red-500 font-semibold">
-            {errors.mail.message}
+    <div className="flex items-center justify-between ">
+      {/* form section  */}
+      <div className="flex items-center justify-center w-[50vw] order-last">
+        <div className="w-[55%]">
+          <h1 className="font-semibold text-4xl">Join us today 🚀</h1>
+          <p className="mt-2 opacity-75">
+            Every great journey begins with a single step. <br /> Sign up now to
+            start shaping your future!
           </p>
-        )}
-        <br />
-        <label className="text-sm font-semibold text-zinc-700" htmlFor="">
-          Pssword
-        </label>
-        <br />
-        <input
-          {...register("password", {
-            required: {
-              value: true,
-              message: "this feild is required",
-            },
-          })}
-          className="outline-none ring-1 ring-zinc-500 rounded-md py-2 px-3 my-2"
-          type="password"
-          placeholder="******"
-        />
-        {errors.password && (
-          <p className="text-xs text-red-500 font-semibold">
-            {errors.password.message}
+
+          <form className="mt-8" onSubmit={handleSubmit(submit)}>
+            {/* first and last name */}
+            <div className="flex gap-2">
+              <fieldset
+                className={`border-2 ${
+                  errors.firstname ? "border-red-500" : "border-green-700"
+                } px-4 rounded-md flex-1`}
+              >
+                <legend
+                  className={`${
+                    errors.firstname ? "text-red-500" : "text-green-700"
+                  } font-semibold`}
+                >
+                  {errors.firstname ? errors.firstname.message : "First name"}
+                </legend>
+                <input
+                  {...register("firstname", {
+                    required: {
+                      value: true,
+                      message: "first name is required",
+                    },
+                  })}
+                  type="text"
+                  className="outline-none py-2 w-full"
+                  placeholder="Rohit"
+                />
+              </fieldset>
+              <fieldset
+                className={`border-2 ${
+                  errors.lastname ? "border-red-500" : "border-green-700"
+                } px-4 rounded-md flex-1`}
+              >
+                <legend
+                  className={`${
+                    errors.lastname ? "text-red-500" : "text-green-700"
+                  } font-semibold`}
+                >
+                  {errors.lastname ? errors.lastname.message : "First name"}
+                </legend>
+                <input
+                  {...register("lastname", {
+                    required: {
+                      value: true,
+                      message: "last name is required",
+                    },
+                  })}
+                  type="text"
+                  className="outline-none py-2 w-full"
+                  placeholder="Sharma"
+                />
+              </fieldset>
+            </div>
+
+            {/* mail address section  */}
+            <div className="mt-4">
+              <fieldset
+                className={`border-2 ${
+                  errors.firstname ? "border-red-500" : "border-green-700"
+                } px-4 rounded-md flex-1`}
+              >
+                <legend
+                  className={`${
+                    errors.firstname ? "text-red-500" : "text-green-700"
+                  } font-semibold`}
+                >
+                  {errors.firstname ? errors.firstname.message : "First name"}
+                </legend>
+                <input
+                  {...register("mail", {
+                    required: {
+                      value: true,
+                      message: "Email is required",
+                    },
+                  })}
+                  type="email"
+                  className="outline-none py-2"
+                  placeholder="Enter your email"
+                />
+              </fieldset>
+            </div>
+
+            {/* password section  */}
+            <div className="mt-4">
+              <fieldset
+                className={`border-2 ${
+                  errors.firstname ? "border-red-500" : "border-green-700"
+                } px-4 rounded-md flex-1`}
+              >
+                <legend
+                  className={`${
+                    errors.firstname ? "text-red-500" : "text-green-700"
+                  } font-semibold`}
+                >
+                  {errors.firstname ? errors.firstname.message : "First name"}
+                </legend>
+                <input
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Provide a password",
+                    },
+                  })}
+                  type="password"
+                  className="outline-none py-2"
+                  placeholder="Enter password"
+                />
+              </fieldset>
+            </div>
+            <p className="text-right font-bold text-green-800 text-sm mt-2">
+              Forgot password?
+            </p>
+            <button
+              className={`bg-green-700 w-full py-2 rounded-md text-white mt-4 ${
+                isSubmitting && "opacity-60 cursor-not-allowed"
+              }`}
+            >
+              {isSubmitting ? "Signing..." : "Sign Up"}
+            </button>
+          </form>
+
+          {/* divider  */}
+          <div className="flex justify-between items-center gap-2 my-6">
+            <div className="ring-1 ring-zinc-400 w-full h-0"></div>
+            <p>or</p>
+            <div className="ring-1 ring-zinc-400 w-full h-0"></div>
+          </div>
+
+          {/* social media login  */}
+          <p className="text-sm font-semibold mb-4">
+            Sign up with open accounts
           </p>
-        )}
-        <br />
-        <button className="w-full bg-black text-white py-2 mt-6 rounded-md">
-          Sign up
-        </button>
-        <p className="text-zinc-700 text-sm mt-3">
-          Already have an account ?{" "}
-          <span
-            onClick={() => navigate("/login")}
-            className="cursor-pointer text-black font-semibold"
-          >
-            Log In
-          </span>{" "}
-        </p>
-      </form>
+          <div className="flex items-center justify-between gap-2">
+            <div className="px-16 bg-green-100 py-1 rounded-md cursor-pointer w-full">
+              <img className="w-8" src={google} alt="" />
+            </div>
+            <div className="px-16 bg-green-100 py-1 rounded-md cursor-pointer w-full">
+              <img className="w-8" src={microsoft} alt="" />
+            </div>
+          </div>
+
+          <p className="text-sm text-center mt-9">
+            Already have an account?{" "}
+            <span
+              onClick={() => navigate("/login")}
+              className="text-green-700 font-semibold cursor-pointer"
+            >
+              Log in
+            </span>{" "}
+          </p>
+        </div>
+      </div>
+
+      {/* image section  */}
+      <div className="w-[50vw] h-screen">
+        <img className="w-full h-full object-cover" src={loginbg} alt="" />
+      </div>
       <ToastContainer />
     </div>
   );
